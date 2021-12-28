d2c6958399d458b594924348e54e31a8bf75ba5b
Sammaya <74440501+sammaya164@users.noreply.github.com>
2021-12-28 15:30:07 +0900

    Update 001.js

--- assets/js/001.js
+++ assets/js/001.js
@@ -5,7 +5,6 @@ var max = document.getElementsByClassName("btn")[1];
 var min = document.getElementsByClassName("btn")[2];
 
 function maximize () {
-alert('max');
 	var post = document.getElementsByClassName("content")[0];
 	var cont = document.getElementsByClassName("post_content")[0];
 	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
